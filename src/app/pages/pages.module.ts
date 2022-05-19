9e184a352fb20322d684a663fb9849833d08570b
Karine <martinskazk@gmail.com>
2022-05-19 01:20:24 -0300

    Criação e estilização do header

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
@@ -6,6 +7,6 @@ import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}
