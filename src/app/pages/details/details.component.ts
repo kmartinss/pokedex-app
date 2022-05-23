1b673a67b099f32403c97923c850671fe196686f
Karine <martinskazk@gmail.com>
2022-05-23 01:29:34 -0300

    Tela de requisição de detalhes do pokemon

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,38 @@
+import { PokeApiService } from './../../service/poke-api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
 
-  constructor() { }
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _pokeApiService: PokeApiService
+  ) {}
 
   ngOnInit(): void {
+    this.pokemon;
   }
 
+  public get pokemon() {
+    const id = this._activatedRoute.snapshot.params['id'];
+
+    const pokemon = this._pokeApiService.getPokemonsByUrl(
+      `${this.urlPokemon}/${id}`
+    );
+
+    const name = this._pokeApiService.getPokemonsByUrl(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
