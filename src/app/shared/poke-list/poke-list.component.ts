8ffeb39994c7901455952d2ae36daf669aa033db
Karine <martinskazk@gmail.com>
2022-05-23 01:16:20 -0300

    Filtro de pokemons \o/

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-  pokemons: any;
+  public pokemons: any;
+  private setPokemons: any;
+
   constructor(private _pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
@@ -16,7 +18,16 @@ export class PokeListComponent implements OnInit {
 
   public getPokemons() {
     this._pokeApiService.listAllPokemons.subscribe((res) => {
-      this.pokemons = res.results;
+      this.setPokemons = res.results;
+      this.pokemons = this.setPokemons;
     });
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.pokemons = filter;
   }
 }
