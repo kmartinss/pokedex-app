dd5ddfeba3938effe666195827d157f3c1a4c62f
Karine <martinskazk@gmail.com>
2022-05-23 00:48:29 -0300

    Consumindo dados da api \o/

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  pokemons: any;
   constructor(private _pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.getPokemons()
+    this.getPokemons();
   }
 
   public getPokemons() {
-    this._pokeApiService.listAllPokemons.subscribe((res) => console.log(res));
+    this._pokeApiService.listAllPokemons.subscribe((res) => {
+      this.pokemons = res.results;
+    });
   }
 }
